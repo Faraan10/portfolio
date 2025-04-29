4a0960ec182c41ce1d4e813c455a98d5049a5a50
Muhammed <faraanshaik19@gmail.com>
2025-04-29 14:57:47 +0530

    work page updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,35 @@
+import notes from "../assets/notes.png";
 import Ecommerce from "../assets/ecommerce.png";
 import TodoList from "../assets/todolist.png";
 import KeeperApp from "../assets/keeperapp.png";
 
 export const projects = [
-	{
-		id: 1,
-		name: "Ecommerce Application",
-		image: Ecommerce,
-		github: "https://github.com/Faraan10/Ecommerce_Frontend",
-		live: "https://ecommerce-frontend-iqxge5zev-muhammed-faraans-projects.vercel.app/",
-	},
-	{
-		id: 2,
-		name: "Todo List App",
-		image: TodoList,
-		github: "https://github.com/Faraan10/Todo_List-Frontend",
-		live: "https://muhammedfaraantodolist.netlify.app/",
-	},
-	{
-		id: 3,
-		name: "Keeper App",
-		image: KeeperApp,
-		github: "https://github.com/Faraan10/Keeper_App-Frontend",
-		live: "https://muhammedfaraankeeperapp.netlify.app/",
-	},
+  {
+    id: 1,
+    name: "Notes Application",
+    image: notes,
+    github: "https://github.com/Faraan10/Notes-App-Frontend",
+    live: "https://notes-app-muhammed-faraans-projects.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Ecommerce Application",
+    image: Ecommerce,
+    github: "https://github.com/Faraan10/Ecommerce_Frontend",
+    live: "https://ecommerce-frontend-iqxge5zev-muhammed-faraans-projects.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Todo List App",
+    image: TodoList,
+    github: "https://github.com/Faraan10/Todo_List-Frontend",
+    live: "https://muhammedfaraantodolist.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Keeper App",
+    image: KeeperApp,
+    github: "https://github.com/Faraan10/Keeper_App-Frontend",
+    live: "https://muhammedfaraankeeperapp.netlify.app/",
+  },
 ];
